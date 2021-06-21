4383142078ecab9115f441efe0df660acddbe219
pegggy <13131514956@163.com>
2021-06-21 17:38:57 +0800

    Initial Commit

---
+++ POSForCustomers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  POSForCustomers
+//
+//  Created by 花逢坂詩織 on 2021/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ POSForCustomers/POSForCustomersApp.swift
@@ -0,0 +1,17 @@
+//
+//  POSForCustomersApp.swift
+//  POSForCustomers
+//
+//  Created by 花逢坂詩織 on 2021/6/21.
+//
+
+import SwiftUI
+
+@main
+struct POSForCustomersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
